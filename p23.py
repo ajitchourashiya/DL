cea48fd046a702c628a3e3ccc5d16759d39a2036
ajitchourashiya <123857539+ajitchourashiya@users.noreply.github.com>
2023-11-04 23:25:13 +0530

    Update p23.py

--- p23.py
+++ p23.py
@@ -1,3 +1,15 @@
+#importing necessary libraries
+import tensorflow as tf
+from tensorflow import keras
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+
 #to see length of training dataset
 len(x_train)
 
